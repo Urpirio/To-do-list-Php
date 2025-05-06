3dc750e9e15c289bc28aae084d1e657486f9857b
UrpirioDev <133790157+Urpirio@users.noreply.github.com>
2025-05-05 22:24:44 -0400

    Se modifico el array en php

--- Data.php
+++ Data.php
@@ -3,7 +3,7 @@ $clase = 'Panel_Control_Buttons';
 
 define('Menu', array(
     "Perfil" => ["index.php","bx bx-user","BtnPerfil"],
-    "List" => ["List.php","bx bx-home","BtnList"],
+    "Lista de tarea" => ["List.php","bx bx-copy-alt","BtnList"],
     "Configuracion" => ["Configuracion.php","bx bx-cog","BtnConfig"],
     "Cerrar sesion" => ["Login.php","bx bx-log-out","BtnCerrarSesion"],
 ))
